72245d8b4e05b8475178917fe198828361b7db45
sagar <sagarg.1988@gmail.com>
2018-08-26 08:00:23 +0530

    Initial setup

---
+++ AsyncNotification/AsyncNotification/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for AsyncNotification project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AsyncNotification.settings")
+
+application = get_wsgi_application()
